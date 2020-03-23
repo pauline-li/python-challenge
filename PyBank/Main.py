f15faf65ddc65ccd1dbcaa9adcc540f8d90f4ef8
Pauline Li <pli@teloslogistics.local>
2020-03-23 02:53:28 -0500

    PyBank Main.py 04

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -2,7 +2,6 @@ import os
 import csv
 
 
-#C:\Users\pli.TELOS\Desktop\Bootcamp\VS Python Code\Lecture3
 
 bank_csv = os.path.join('../PyBank/budget_data.csv')
 
@@ -15,7 +14,9 @@ with open(bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
      
-
+    increase_list =[] 
+    increase_date =[] 
+    grt_increase = 0
     FirstFlag = True   
 
     #for x in bank_csv
@@ -28,21 +29,39 @@ with open(bank_csv, 'r') as csvfile:
             min_profitloss = profitloss 
             FirstFlag = False
          total = total + profitloss
-         row_count += 1   #
-
-    max_date = date
-    max_profitloss = profitloss
-  
+        #  row_count += 1   
+         increase_list.append(profitloss)
+         increase_date.append(date)
 
 
+    list_length = len(increase_date)-1
 
- 
+
+        
     print(f"Financial Analysis") 
     print(f"----------------------------".center(24," ")) 
-    print(f"Total Months: {str(row_count)}")
+    print(f"Total Months: {len(increase_date)}")
     print(f"Total: ${str(total)}")
-    print(f"Min Date: {min_date} Max Date: {max_date}")
-    print(f"Average Change: ${str(round((max_profitloss-min_profitloss)/(row_count-1), 2))}")  
+    print(f"Min Date: {increase_date[0]} Max Date: {increase_date[-1]}")
+    print(f"Average Change: ${str(round((increase_list[list_length]-increase_list[0])/list_length, 2))}")
+    print(f"{increase_list[75]}")
+
+
+
+csvwriter = output_path = os.path.join("../PyBank/PyBank_Output.csv")  # Write and create file in here
+
+with open(csvwriter, 'w') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile)
+    csvfile.write("\n".join("Financial Analysis"))
+
+    # Write the first row (column headers)
+    csvwriter.writerow("Financial Analysis")
+
+    # Write the second row
+    csvwriter.writerow(f"Total Months: {str(row_count)}")
+
 
 
  
