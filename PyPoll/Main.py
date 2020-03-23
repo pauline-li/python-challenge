890c5d3e60eafd75202b96edb7473a712fce7d47
Pauline Li <pli@teloslogistics.local>
2020-03-23 12:01:41 -0500

    PyPoll Main.py 03

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -5,22 +5,34 @@ poll_csv = os.path.join('../PyPoll/election_data.csv')
 # total = 0
 #Voter ID, County, and Candidate. 
 
+def unique(candidatelist):
+    unique_list=[]
+
+    for x in candidatelist:
+        if x not in unique_list:
+            unique_list.append(x)
+    
+    return  unique_list
+    # for x in unique_list: 
+    #     print(f"{x}")
+
 with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',') # Split the data on commas
     header = next(csvreader)
      
     voter_id =[] 
     county =[]
-    candidate =[]  
+    candidate =[] 
+    
 #     grt_increase = 0
 #     FirstFlag = True   
-     
-   
+#sortdata = itertools.groupby(sortdata, operator.itemgetter('candidate'))
+
     for poll_data in csvreader:
         voter_id_x = str(poll_data[0])
         county_x = str(poll_data[1])
         candidate_x = str(poll_data[2])
-
+        
 
 #          if FirstFlag == True:
 #             min_date = date
@@ -40,7 +52,9 @@ with open(poll_csv, 'r') as csvfile:
     print(f"----------------------------".center(24," ")) 
     print(f"Total Votes: {list_length}")
     print(f"----------------------------".center(24," ")) 
-
+    #for candidate_x in candidate: 
+    print(f"Unique Candidate: {unique(candidate)}")
+   
 
 
 # Election Results
