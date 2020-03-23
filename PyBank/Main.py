c14b3e7d4170994b2c630af38ecebf2725900c07
Pauline Li <pli@teloslogistics.local>
2020-03-23 03:54:11 -0500

    PyBank Main.py 06

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -38,13 +38,17 @@ with open(bank_csv, 'r') as csvfile:
     list_length = len(increase_date)-1
     greatest_inc= increase_list[1]-increase_list[0] 
     greatest_dec= increase_list[1]-increase_list[0] 
+    incr_month=0
+    decr_month=0
 
     for i in range(1,list_length+1):
         change=increase_list[i] - increase_list[i-1] 
         if change > greatest_inc:
-            greatest_inc = change  
+            greatest_inc = change 
+            incr_month = i 
         if change < greatest_dec:
-            greatest_dec = change  
+            greatest_dec = change 
+            decr_month = i
 
 
 
@@ -59,10 +63,12 @@ with open(bank_csv, 'r') as csvfile:
     print(f"----------------------------".center(24," ")) 
     print(f"Total Months: {len(increase_date)}")
     print(f"Total: ${str(total)}")
-    print(f"Min Date: {increase_date[0]} Max Date: {increase_date[-1]}")
+    # print(f"Min Date: {increase_date[0]} Max Date: {increase_date[-1]}")
     print(f"Average Change: ${str(round((increase_list[list_length]-increase_list[0])/list_length, 2))}")
-    print(f"{greatest_inc} greatest decrease {greatest_dec}")
-   
+    print(f"Greatest Increase in Profits: {increase_date[incr_month]} (${greatest_inc})")
+    print(f"Greatest Decrease in Profits: {increase_date[decr_month]} (${greatest_dec})")
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
 
 
 csvwriter = output_path = os.path.join("../PyBank/PyBank_Output.csv")  # Write and create file in here
