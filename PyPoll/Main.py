ee0ec071e583333b6e630fc7ee872d2adc18975b
Pauline Li <pli@teloslogistics.local>
2020-03-23 11:21:50 -0500

    PyPoll Main.py 02

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -43,11 +43,11 @@ with open(poll_csv, 'r') as csvfile:
 
 
 
-# •	Election Results
+# Election Results
 # •	-------------------------
 # •	Total Votes: 3521001
 # •	-------------------------
-# •	Khan: 63.000% (2218231)
+# •	Khan: 63.000% (2218231
 # •	Correy: 20.000% (704200)
 # •	Li: 14.000% (492940)
 # •	O'Tooley: 3.000% (105630)
