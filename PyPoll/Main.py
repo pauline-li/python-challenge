7fee19d6b166fdd538a1df20289f1d28ca45c188
Pauline Li <pli@teloslogistics.local>
2020-03-23 23:12:11 -0500

    PyPoll Main.04

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -2,71 +2,59 @@ import os
 import csv
 
 poll_csv = os.path.join('../PyPoll/election_data.csv')
-# total = 0
-#Voter ID, County, and Candidate. 
 
-def unique(candidatelist):
-    unique_list=[]
-
-    for x in candidatelist:
-        if x not in unique_list:
-            unique_list.append(x)
-    
-    return  unique_list
-    # for x in unique_list: 
-    #     print(f"{x}")
 
 with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',') # Split the data on commas
     header = next(csvreader)
      
-    voter_id =[] 
-    county =[]
-    candidate =[] 
     
-#     grt_increase = 0
-#     FirstFlag = True   
-#sortdata = itertools.groupby(sortdata, operator.itemgetter('candidate'))
-
+    unique_candidate_name =[]
+    voterID=[] 
+    vote_sum=[]
+    winner_index=0
+  
+    
     for poll_data in csvreader:
-        voter_id_x = str(poll_data[0])
-        county_x = str(poll_data[1])
-        candidate_x = str(poll_data[2])
-        
-
-#          if FirstFlag == True:
-#             min_date = date
-#             min_profitloss = profitloss 
-#             FirstFlag = False
-#          total = total + profitloss
-        voter_id.append(voter_id_x)
-        county.append(county_x)
-        candidate.append(candidate_x)
-
-    list_length = len(voter_id)
-
-
-
-        
-    print(f"Election Results") 
+        voterID.append(poll_data[0]) #reading column 1 data Voters ID
+        candidate_name=(poll_data[2]) #reading column 3 data candidate
+        if candidate_name in unique_candidate_name: #if is existing candidate
+            candidate_index=unique_candidate_name.index(candidate_name)#index where 
+            vote_sum[candidate_index] = vote_sum[candidate_index] +1
+        else:
+            unique_candidate_name.append(candidate_name)
+            vote_sum.append(1)
+    
+    print(f"Election Results")
     print(f"----------------------------".center(24," ")) 
-    print(f"Total Votes: {list_length}")
+    print(f"Total Votes: {sum(vote_sum)}")
+    print(f"----------------------------".center(24," "))
+
+    for i in range(0,len(unique_candidate_name)):   
+        print(f"{unique_candidate_name[i]}: {(vote_sum[i]/sum(vote_sum))*100:.03f}% ({vote_sum[i]})  ") # print and format each vote percentage
+
+    for i in range(0,len(unique_candidate_name)-1):  
+        if vote_sum[i+1] > vote_sum[i]:
+         winner_index=i+1 
+
     print(f"----------------------------".center(24," ")) 
-    #for candidate_x in candidate: 
-    print(f"Unique Candidate: {unique(candidate)}")
+    print(f"Winner: {unique_candidate_name[winner_index]}")
+    print(f"----------------------------".center(24," "))
+
+
    
-
+       
 
 # Election Results
-# •	-------------------------
-# •	Total Votes: 3521001
-# •	-------------------------
-# •	Khan: 63.000% (2218231
-# •	Correy: 20.000% (704200)
-# •	Li: 14.000% (492940)
-# •	O'Tooley: 3.000% (105630)
-# •	-------------------------
-# •	Winner: Khan
+# -------------------------
+# Total Votes: 3521001
+# -------------------------
+# Khan: 63.000% (2218231
+# Correy: 20.000% (704200)
+# Li: 14.000% (492940)
+# O'Tooley: 3.000% (105630)
+# -------------------------
+# Winner: Khan
 # -------------------------
 
 
