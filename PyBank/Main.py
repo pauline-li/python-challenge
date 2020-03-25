3910e2eb00172fdf7e95e851ce8514cf3bdf9fb9
Pauline Li <pli@teloslogistics.local>
2020-03-25 10:21:19 -0500

    PyBank Main.Py 08

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -8,7 +8,7 @@ with open(bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',') # Split the data on commas
     header = next(csvreader)
      
-    increase_list =[] 
+    change_to_profit_loss =[] 
     increase_date =[] 
     grt_increase = 0
     FirstFlag = True   
@@ -22,18 +22,18 @@ with open(bank_csv, 'r') as csvfile:
             min_profitloss = profitloss 
             FirstFlag = False
          total = total + profitloss
-         increase_list.append(profitloss)
+         change_to_profit_loss.append(profitloss)
          increase_date.append(date)
 
 
     list_length = len(increase_date)-1
-    greatest_inc= increase_list[1]-increase_list[0] 
-    greatest_dec= increase_list[1]-increase_list[0] 
+    greatest_inc= change_to_profit_loss[1]-change_to_profit_loss[0] 
+    greatest_dec= change_to_profit_loss[1]-change_to_profit_loss[0] 
     incr_month=0
     decr_month=0
 
     for i in range(1,list_length+1):
-        change=increase_list[i] - increase_list[i-1] 
+        change=change_to_profit_loss[i] - change_to_profit_loss[i-1] 
         if change > greatest_inc:
             greatest_inc = change 
             incr_month = i 
@@ -46,25 +46,26 @@ with open(bank_csv, 'r') as csvfile:
     print(f"----------------------------".center(24," ")) 
     print(f"Total Months: {len(increase_date)}")
     print(f"Total: ${str(total)}")
-    print(f"Average Change: ${str(round((increase_list[list_length]-increase_list[0])/list_length, 2))}")
+    print(f"Average Change: ${str(round((change_to_profit_loss[list_length]-change_to_profit_loss[0])/list_length, 2))}")
     print(f"Greatest Increase in Profits: {increase_date[incr_month]} (${greatest_inc})")
     print(f"Greatest Decrease in Profits: {increase_date[decr_month]} (${greatest_dec})")
 
 
-csvwriter = output_path = os.path.join("../PyBank/PyBank_Output.csv")  # Write and create file in here
+csv_writer = output_path = os.path.join("../PyBank/PyBank_Output.csv")  # Write and create file in here
 
-with open(csvwriter, 'w') as csvfile:
+with open(csv_writer, 'w') as csvfile:
 
     # Initialize csv.writer
-    csvwriter = csv.writer(csvfile)
-    csvfile.write("\n".join("Financial Analysis"))
+    csv_writer = csv.writer(csvfile)
+    #csvfile.write("\n".join("Financial Analysis"))
 
     # Write the first row (column headers)
-    csvwriter.writerow("Financial Analysis")
+   
+    csv_writer.writerow(["Financial Analysis"])
+    csv_writer.writerow(["----------------------------".center(24," ")])
+    csv_writer.writerow([f"Total Months: {len(increase_date)}"])
+    csv_writer.writerow([f"Total: ${str(total)}"])
+    csv_writer.writerow([f"Average Change: ${str(round((change_to_profit_loss[list_length]-change_to_profit_loss[0])/list_length, 2))}"])
+    csv_writer.writerow([f"Greatest Increase in Profits: {increase_date[incr_month]} (${greatest_inc})"])
+    csv_writer.writerow([f"Greatest Decrease in Profits: {increase_date[decr_month]} (${greatest_dec})"])
 
-    # Write the second row
-    csvwriter.writerow(f"Total Months: {str(list_length)}")
-
-
-
- 
